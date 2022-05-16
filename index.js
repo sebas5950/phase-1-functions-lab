77e1d1ce2052899df8140f36179e99254ba0ad83
Alex Guanipatin <sebas5950@gmail.com>
2022-05-16 15:48:17 -0400

    commit tree aedf322349e2415312241345f7f7d8cfec2845e4

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUp) {
+    return Math.abs(pickUp - 42);
+}
+
+function distanceFromHqInFeet(origin) {
+    return (distanceFromHqInBlocks(origin) * 264)
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = Math.abs(start - destination) * 264;
+    if ( distance < 400) {
+        return 0
+    } 
+
+       else if (distance >= 2500 ) {
+        return 'cannot travel that far'
+    }
+
+    else if ( distance < 2000 ) {
+        return ((distance -400) * .02 );
+    }
+
+    else if (distance > 2000 ) {
+        return 25;
+
+    }
+ 
+    }
+
+
+
+
